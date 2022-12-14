60d9f192e61c36aea4f6930dba307bbab168ab97
fajar  firdaus <firdaus.fajar03@gmail.com>
2022-12-14 16:45:00 +0700

    Update Kontak

---
+++ html/kontak.php
@@ -0,0 +1,71 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Greenside</title>
+    <link rel="stylesheet" type="text/css" href="../../css/style-kontak.css">
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+</head>
+
+<body>
+
+
+    <!-- Navbar -->
+    <header>
+
+        <div class="container">
+
+            <h1><a href="#"><i class="fas fa-ring"> GreenSide </i></a></h1>
+            <h1><i class="fas fa-ring"> GreenSide </i></h1>
+            <ul>
+                <li><a href="../index.php">Home</a></li>
+                <!-- Menambahkan class active -->
+                <li><a href="paket.php">Paket</a></li>
+                <li><a href="../galeri.php">Galeri</a></li>
+                <li><a href="pemesanan.php">Pesanan</a></li>
+                <li><a href="kontak.php">Kontak</a></li>
+            </ul>
+        </div>
+    </header>
+
+    <div class="kontak">
+        <div class="container">
+            <h1>
+                <marquee>Terimakasih Sudah Melihat Lihat Harap Anda Segera Menghubungi Kami</marquee>
+            </h1>
+            <u>
+                <h2> Kontak </h2>
+            </u>
+            <br>
+            <br>
+            <a href="https://instagram.com/acep_handika?utm_medium=copy_link" class="floatt" target="_blank">
+                <i class="fab fa-instagram my-floatt"></i>
+            </a>
+            <p>Jika anda memiliki pertanyaan, silahkan hubgungi kami!!</p>
+            <br>
+            <br>
+            <a href="https://api.whatsapp.com/send?phone=6282316167554&text=Hallo%20saya%20mau%20order!!" class="float"
+                target="_blank">
+                <i class="fa fa-whatsapp my-float"></i>
+            </a>
+
+            <p>Alamat : Padasuka,Kec. Cimahi Tengah, Kota Cimahi, Jawa Barat</p>
+            <p>Email : acephandika27@gmail.com</p>
+            <p>Terimakasih
+                <br>
+                <br>
+                <iframe
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1980.5126949259507!2d107.52585575657692!3d-6.887562111791021!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2e68e5c8d95a1a6b%3A0xd7e69284025bdc4d!2sKampus%20unjani!5e0!3m2!1sid!2sid!4v1670834195500!5m2!1sid!2sid"
+                    width="1230" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+        </div>
+    </div>
+    <div class="footer">
+        <small>Copyright &copy; 2022 - Greenside. All Right Reserved</small>
+    </div>
+
+</body>
+
+</html>
