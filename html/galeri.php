3f27465ba284427a4ca3edd3ef740c1d632ee61e
Naufal Indra <indranaufal322@gmail.com>
2022-12-14 16:57:31 +0700

    Update Menu Galeri

---
+++ html/galeri.php
@@ -0,0 +1,113 @@
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Greenside</title>
+    <link rel="stylesheet" type="text/css" href="../css/style-galery.css">
+    <link rel="stylesheet" type="text/css"
+        href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="../css/lightbox.min.css">
+</head>
+
+<body>
+
+
+    <!-- Navbar -->
+    <header>
+
+        <div class="container">
+
+            <h1><a href="#"><i class="fas fa-ring"> GreenSide </i></a></h1>
+            <h1><i class="fas fa-ring"> GreenSide </i></h1>
+            <ul>
+                <li><a href="index.php">Home</a></li>
+                <!-- Menambahkan class active -->
+                <li><a href="user/paket.php">Paket</a></li>
+                <li><a href="#">Galeri</a></li>
+                <li><a href="user/pemesanan.php">Pesanan</a></li>
+                <li><a href="user/kontak.php">Kontak</a></li>
+            </ul>
+        </div>
+    </header>
+
+
+    <!-- Slideshow container -->
+    <div class="slideshow-container">
+
+        <!-- Full-width images with number and caption text -->
+        <div class="mySlides fade">
+            <div class="numbertext"></div>
+            <img src="../gambar/kk.jpg" style="width:100%">
+            <div class="text">Silakan Dilihat Terlebih Dahulu</div>
+        </div>
+
+        <div class="mySlides fade">
+            <div class="numbertext"></div>
+            <img src="../gambar/k.jpg" style="width:100%">
+            <div class="text">Kami Menyediakan Hasil Kami</div>
+        </div>
+
+        <div class="mySlides fade">
+            <div class="numbertext"></div>
+            <img src="../gambar/kkk.jpg" style="width:100%">
+            <div class="text">Semoga Anda Menikmatinya</div>
+        </div>
+
+        <!-- Next and previous buttons -->
+        <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+        <a class="next" onclick="plusSlides(1)">&#10095;</a>
+    </div>
+    <br>
+
+    <!-- The dots/circles -->
+    <div style="text-align:center">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
+    </div>
+
+    <h1 style="text-align: center; margin: 50px 0px 0px 0px; color: #4d4d4d;"> OUR GALERIES</h1>
+
+    <section>
+        <div class="galeri">
+            <a class="example-image-link" href="../gambar/2.jpeg" data-lightbox="example-set"
+                data-title="Contoh Foto Terbaik Kami"><img class="example-image" src="../gambar/2.jpeg" alt="" /></a>
+            <a class="example-image-link" href="../gambar/3.jpeg" data-lightbox="example-set"
+                data-title="Contoh Foto Terbaik Kam"><img class="example-image" src="../gambar/3.jpeg" alt="" /></a>
+            <a class="example-image-link" href="../gambar/5.jpeg" data-lightbox="example-set"
+                data-title="Contoh Foto Terbaik Kam"><img class="example-image" src="../gambar/5.jpeg" alt="" /></a>
+            <a class="example-image-link" href="../gambar/1.jpeg" data-lightbox="example-set"
+                data-title="Contoh Foto Terbaik Kam"><img class="example-image" src="../gambar/1.jpeg" alt="" /></a>
+            <a class="example-image-link" href="../gambar/9.jpeg" data-lightbox="example-set"
+                data-title="Contoh Foto Terbaik Kam"><img class="example-image" src="../gambar/9.jpeg" alt="" /></a>
+            <a class="example-image-link" href="../gambar/4.jpeg" data-lightbox="example-set"
+                data-title="Contoh Foto Terbaik Kam"><img class="example-image" src="../gambar/4.jpeg" alt="" /></a>
+            <a class="example-image-link" href="../gambar/8.jpeg" data-lightbox="example-set"
+                data-title="Contoh Foto Terbaik Kam"><img class="example-image" src="../gambar/8.jpeg" alt="" /></a>
+            <a class="example-image-link" href="../gambar/14.jpeg" data-lightbox="example-set"
+                data-title="Contoh Foto Terbaik Kam"><img class="example-image" src="../gambar/14.jpeg" alt="" /></a>
+            <a class="example-image-link" href="../gambar/15.jpeg" data-lightbox="example-set"
+                data-title="Contoh Foto Terbaik Kam"><img class="example-image" src="../gambar/15.jpeg" alt="" /></a>
+
+        </div>
+    </section>
+
+    <br>
+    <br>
+    <br>
+    <a href="https://instagram.com/acep_handika?utm_medium=copy_link" class='tombol'>Visit Instagram</a>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <br>
+    <div class="footer">
+        <small>Copyright &copy; 2022 - Greenside. All Right Reserved</small>
+    </div>
+    <script src="../js/lightbox-plus-jquery.min.js"></script>
+</body>
+
+</html>
